86af58f209b232d1f5a32df453439e1799ca3b88
Langlois, Vince Thomas <vtlanglo@iu.edu>
2021-09-03 22:20:47 -0400

    Refactored code to work with final exercise

--- main.py
+++ main.py
@@ -4,46 +4,84 @@ import json
 import os
 assert sys.version_info >= (3,9), "This script requires at least Python 3.9"
 
-#Get all json filenames within the json folder
-path_to_json_files = "json/"
-json_files = [pos_json for pos_json in os.listdir(path_to_json_files) if pos_json.endswith('.json')]
+def select_game():
+  #Get all json filenames within the json folder
+  path_to_json_files = "json/"
+  json_files = [pos_json for pos_json in os.listdir(path_to_json_files) if pos_json.endswith('.json')]
+  
+  response = ""
+  #Allow player to select json file
+  while True:
+    if response in json_files:
+      break
+    print("Select a Game File:")
+    print(json_files)
+    response = input()  
+  #get the file, create a world, return the world
+  response = "json/"+response
+  file = open(response, )
+  world = json.load(file)
+  return world
 
-response = ""
-#Allow player to select json file
-while True:
-  if response in json_files:
-    break
-  print("Select a Game File:")
-  print(json_files)
-  response = input()  
-response = "json/"+response
-file = open(response, )
-world = json.load(file)
 
-current = world["passages"][0]["name"]
-response = ""
-while True:
-    # Find passage (update)
-    current_location = {}
-    # Go thru all passages, find neccessary passage
-    for passage in world["passages"]:
-      if passage["name"] == current:
-        current_location = passage
-        break
-    # Display passage (render the world)
+# ----------------------------------------------------------------
+
+def find_current_location(location_label):
+	if "passages" in world:
+		for passage in world["passages"]:
+			if location_label == passage["name"]:
+				return passage
+	return {}
+
+# ----------------------------------------------------------------
+
+def render(current_location, score, moves):
+  if "name" in current_location and "cleanText" in current_location:
+	 # Display passage (render the world)
+    print("Moves: " + str(moves) + " | Score: " + str(score))
     print("\n" + current_location["name"] + " - " + current_location["cleanText"])
     # Print all passage links
     for link in current_location["links"]:
-      print(link["linkText"] + " - " + link["passageName"])
-    # Ask for response (get input)
-    response = input("\nWhere do you want to go? ")
-    if response == "quit":
-        break
-    # Check if passage link exists in current passage; else redo loop
-    for link in current_location["links"]:
-      if(response == link["linkText"] ):
-        current = link["passageName"]
-        break
-    else:
-      print("Option not found")
+      print(" ->" + link["linkText"] + " - " + link["passageName"])
+    
+
+def get_input():
+	response = input("Enter option: ")
+	return response.upper().strip()
+
+def update(current_location, location_label, response):
+  #if there is no response, return location_label argument
+	if response == "":
+		return location_label
+  # see if there are links in the current_location
+	if "links" in current_location:
+    #for each link, see if response matches a link
+	  for link in current_location["links"]:
+		  if(response == link["linkText"]):
+			  return link["passageName"]
+	else:
+		print("Option not found.")
+		return location_label
+
+
+# ----------------------------------------------------------------
+
+world = select_game()
+location_label = world["passages"][0]["name"]
+current_location = {}
+response = ""
+score = 0
+moves = 0
+
+while True:
+  if response == "QUIT":
+    break
+  if "score" in current_location:
+    score+=current_location["score"]
+  location_label = update(current_location, location_label, response)
+  current_location = find_current_location(location_label)
+  render(current_location, score, moves)
+  response = get_input()
+  moves+=1
+   
 
